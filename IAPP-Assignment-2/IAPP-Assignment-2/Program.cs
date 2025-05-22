b86f0641726a56322b68d38558ee0f987965772c
Tinotenda Mhedziso <tinomhedziso22@gmail.com>
2025-05-22 12:51:28 +0200

    Added files

---
+++ IAPP-Assignment-2/IAPP-Assignment-2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAPP_Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public sealed class HotelRoomManager
+    { 
+        private static volatile HotelRoomManager hotelRoomManager;
+        private static object _lock = new object();
+
+        private HotelRoomManager()
+        {
+            Console.WriteLine("Welcome to the Hotel Room Management System");
+        }
+
+        public static HotelRoomManager GetInstance()
+        {
+            if(hotelRoomManager == null)
+            {
+                lock (_lock)
+                {
+                    if (hotelRoomManager == null)
+                    {
+                        hotelRoomManager = new HotelRoomManager();
+                    }
+                }
+            }
+
+            return hotelRoomManager;
+        }
+    }
+
+    public abstract class Room
+    {
+
+
+    }
+
+}
---
+++ IAPP-Assignment-2/IAPP-Assignment-2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IAPP-Assignment-2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nelson Mandela University")]
+[assembly: AssemblyProduct("IAPP-Assignment-2")]
+[assembly: AssemblyCopyright("Copyright © Nelson Mandela University 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cab0900e-cca5-4fb4-82be-f6c191be91d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
